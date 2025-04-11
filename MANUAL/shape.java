0d5d8a978827e48304b0c2477b47c7a03192dcd8
Manoj4681 <manojmadiraju64@gmail.com>
2025-04-11 23:14:46 +0530

    Add files via upload

---
+++ MANUAL/shape.java
@@ -0,0 +1,52 @@
+abstract class Shape3D {
+    abstract double calculateVolume();
+    abstract double surfaceArea();
+}
+
+class Sphere extends Shape3D {
+    int r;
+
+    public Sphere(int r) {
+        this.r = r;
+    }
+
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(r, 3);
+    }
+
+    public double surfaceArea() {
+        return 4 * Math.PI * Math.pow(r, 2);
+    }
+}
+
+class Cube extends Shape3D {
+    int a;
+
+    public Cube(int a) {
+        this.a = a;
+    }
+
+    public double calculateVolume() {
+        return Math.pow(a, 3);
+    }
+
+    public double surfaceArea() {
+        return 6 * Math.pow(a, 2);
+    }
+}
+
+ class shape {
+    public static void main(String[] args) {
+        System.out.println("did by: M.N.H.O.K.MANOJ  from cse-c roll no : cse24214");
+        Sphere obj = new Sphere(6);
+        Cube obj1 = new Cube(8);
+
+        System.out.println("Sphere Volume: " + obj.calculateVolume());
+        System.out.println("Sphere Surface Area: " + obj.surfaceArea());
+
+        System.out.println("Cube Volume: " + obj1.calculateVolume());
+        System.out.println("Cube Surface Area: " + obj1.surfaceArea());
+    }
+}
+
+
